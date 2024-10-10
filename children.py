aab55eab9f596a816666f7ea7b1b6dce6bca8731
evrard sibomana <evrard.sibomana@gmail.com>
2024-10-10 13:54:21 +0200

    add1

---
+++ children.py
@@ -0,0 +1,18 @@
+class Children :
+    def __init__(self , name) :
+        self.name = name
+        self.wich = [ ]
+        self.adress = True
+
+    def add_wich(self , wish) :
+        
+
+    def display_wish(self) :
+        for wish in self.wich :
+            print( f"Your wish is {wish}" )
+
+
+child = Children( "test" )
+child.add_wich( "car" )
+child.add_wich( "poppy" )
+child.display_wish( )
