0e8d4b68c9c5210558dfd82759c91e220fa924d8
A-Renie <renie.arthur@gmail.com>
2024-10-10 13:53:16 +0200

    debut class Adress, debut du dictionnaire de villes

---
+++ Address.py
@@ -0,0 +1,14 @@
+from city_dictionnary import city_dictionnary
+import random
+
+class Address:
+    def __init__(self,street_name):
+        self.street_name = street_name
+        self.city,self.distance_from_north_pole = random.choice(list(city_dictionnary.items()))
+
+
+test = Address("84 Digue de Cuesmes")
+
+print (test.street_name)
+print (test.city)
+print (test.distance_from_north_pole)
---
+++ Exercice_client.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((1920, 1080))
+
+screen.fill((128,0,128))
+
+
+running = True
+while running:
+    pygame.display.flip()
+
